8a1afab8fd943d8d524a7314f6f0746eb60b1121
quangnvph26060 <quangnvph26060@fpt.edu.vn>
2024-06-09 11:29:57 +0700

    random password

---
+++ User/random-password.php
@@ -0,0 +1,20 @@
+<?php
+class password {
+    public function generatePassword(): string
+    {
+        $length     = 6;
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $password   = '';
+   
+        do {
+            $password = '';
+            for ($i = 0; $i < $length; $i++) {
+                $password .= $characters[rand(0, strlen($characters) - 1)];
+            }
+        } while (!preg_match('/^(?=.*[0-9])(?=.*[a-zA-Z])/', $password));
+   
+        return $password;
+    }
+}
+
+
